0be6adda489cc283bdad633fd7152478bf1e247c
radenir <radenirdsd@yahoo.com>
2014-08-27 15:56:45 +0200

    Create usage.php

---
+++ usage.php
@@ -0,0 +1,13 @@
+<?php
+
+include 'hello.php';
+
+echo HelloWorld::getMessage();
+
+/* That is my first GitHub project
+@ I'm just trying to work everything out.
+@ Hopefully, every next project I will have made
+@ will be more complex and creative.
+
+@ this is an example usage of class HelloWorld.
+*/
