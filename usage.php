75e66e7dd2ba3642387431810a352b5563ab56a7
radenir <radenirdsd@yahoo.com>
2014-09-20 14:47:48 +0200

    Update usage.php

--- usage.php
+++ usage.php
@@ -6,7 +6,7 @@ echo HelloWorld::getMessage();
 
 /* That is my first GitHub project
 @ I'm just trying to work everything out.
-@ Hopefully, every next project I will have made
+@ Hopefully, every next project I'm going to make
 @ will be more complex and creative.
 
 @ this is an example usage of class HelloWorld.
